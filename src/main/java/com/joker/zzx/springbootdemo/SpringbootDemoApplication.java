2b794756443492bc878cf8572f8da44183c71e39
joker <441759389@163.com>
2019-11-13 18:21:23 +0800

    first commit

---
+++ src/main/java/com/joker/zzx/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.joker.zzx.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+}
